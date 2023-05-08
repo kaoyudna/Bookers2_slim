455613c5bc3289e3b68221d10dfefc3d99834b35
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-05-08 08:16:48 +0000

    [Add]

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,7 +1,15 @@
 class Public::RelationshipsController < ApplicationController
-  
+
   def create
-    @user = User.find(params[:user_id])
-    User.follow(@user)
+    user = User.find(params[:user_id])
+    current_user.followings << user
+    redirect_to request.referer
   end
+
+  def destroy
+    user = User.find(params[:user_id])
+    current_user.followings.where(user_id: user.id).destroy
+    redirect_to request.referer
+  end
+
 end
