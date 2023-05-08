337ed0436a550358d53372022ad46ebd7bc9a58b
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-05-08 15:04:23 +0000

    [Add]

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -14,10 +14,14 @@ class Public::RelationshipsController < ApplicationController
 
   def following
     @user = User.find(params[:user_id])
+    @followings = @user.followings
+    @book = Book.new
   end
 
   def follower
     @user = User.find(params[:user_id])
+    @followers = @user.followers
+    @book = Book.new
   end
 
 end
